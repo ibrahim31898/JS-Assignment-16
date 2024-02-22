b4acea051afca35735b527c8723277b4f597a4a1
Muhammad Ibrahim Jan <ibrahimjan31898@gmail.com>
2024-02-22 09:09:23 +0500

    Add HTML CSS JavaScript work to github repo

---
+++ app.js
@@ -0,0 +1,207 @@
+// alert (1010)
+// document.write("ibrahim")
+// alert(typeof 50)
+// console.log(typeof "100")
+// console.log(typeof false)
+// console.log(typeof true)
+
+
+
+// var x = "qwerty"
+// var y = "asdfgh"
+// var z = "zxcvb"
+
+// console.log(x + " " + y + " " + z)
+
+// var number = 50;
+
+// alert(number)
+// document.write(number)
+// console.log(number)
+
+// var age = "age";
+
+// console.log(typeof age)
+
+// var firstname = "mr";
+// var lastname = "joe";
+
+// document.write("Firstname is:" , firstname , "and my last name is:" , <i></i> , lastname)
+
+
+// var firstname = "Ibrahim";
+
+// var lastname ="Jan";
+
+// var age = 25;
+
+// var institute = "Jawan Pakistan";
+
+// var cnic = 42201783934545;
+
+// document.write()
+
+
+
+// Assignment 16:
+
+// Chapter 1 ALERTS:
+
+// 1.
+
+alert("Hello")
+
+// 2.
+
+alert("Error! Please enter a valid password.")
+
+// 3.
+
+alert("Welcome to JS Land... \n Happy Coding")
+
+// 4.
+
+alert("Welcome to JS Land...")
+
+alert("Happy Coding! \n Prevent this page from creating additional dialogs")
+
+// 5.
+
+alert("Hello... I can run JS through my web browser's console")
+
+// 6. and 7. on html page
+
+
+// Chapter 2 Variables for Strings:
+
+// 1.
+var username;
+
+// 2. 
+var myName = "John Doe";
+
+// 3. 
+var message;
+message = "Hello World";
+alert(message);
+
+// 4. 
+var studentName = "Alice";
+var studentAge = 20;
+var studentGrade = "A";
+alert("Name: " + studentName + "\nAge: " + studentAge + "\nGrade: " + studentGrade);
+
+// 5.
+var alertMessage = "Welcome to JavaScript";
+alert(alertMessage);
+
+// 6. 
+var email = "example@example.com";
+alert("My email address is: " + email);
+
+// 7. 
+var book = "A smarter way to learn JavaScript";
+alert("I am currently reading the book: " + book);
+
+// 8. 
+document.write("This is displayed in the browser through JavaScript.");
+
+// 9. 
+var messageToShow = "This is a sample message to be displayed.";
+alert(messageToShow);
+document.write(messageToShow);
+
+
+// Chapter 2 VARIABLES FOR NUMBERS: 
+
+// 1. 
+var age = 30;
+alert("My age is: " + age);
+
+// 2. 
+var numberOfVisits = 5; 
+document.write("You have visited this site " + numberOfVisits + " times.");
+
+// 3. 
+var birthYear = 1990;
+document.write("My birth year is: " + birthYear);
+
+// 4. 
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
+
+// Chapter 1-3:
+
+// Chapter 1 (Alerts)
+
+// 1. 
+// I. 
+alert("John");
+// II. 
+alert("Doe");
+// III. 
+alert("john.doe@example.com");
+// IV. 
+alert("123-456-7890");
+// V. 
+alert("mypassword123");
+
+// 2. 
+alert("You're learning JavaScript!");
+
+// 3. 
+alert("Welcome to our website!");
+
+// Chapter 2 (Variables for string)
+
+// 1. 
+var myVariableName;
+
+// 2. 
+var myVariable;
+myVariable = "Hello, world!";
+
+// 3. 
+var teamName = "The Rockets";
+alert(teamName);
+
+// 4. 
+var bestMan = "Charlie";
+bestMan = "David";
+
+// Chapter 3 (Variables for numbers)
+
+// 1. 
+var caseQty;
+
+// 2. 
+caseQty = 144;
+
+// 3. 
+var num = 9;
+
+// 4. 
+var sumVariable;
+sumVariable = 5 + 3;
+
+// 5.
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+// 
+
+// 6.
+var numberVariable = 10;
+numberVariable += 5;
+
+
+
+
+
+
+
+
+
